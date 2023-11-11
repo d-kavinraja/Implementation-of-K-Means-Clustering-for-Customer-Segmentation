779f36545ef47648bbc0ff3ae80bbf667d7934cf
Kavinraja.D <119875375+d-kavinraja@users.noreply.github.com>
2023-11-11 17:14:19 +0530

    Create program.py

---
+++ program.py
@@ -0,0 +1,34 @@
+## Kavinraja D (212222240047)
+import pandas as pd
+import matplotlib.pyplot as plt
+data=pd.read_csv("Mall_Customers.csv")
+data.head()
+data.info()
+data.isnull().sum()
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1,11):
+  kmeans = KMeans (n_clusters = i, init ="k-means++")
+  kmeans.fit(data.iloc[:,3:])
+  wcss.append(kmeans.inertia_)
+plt.plot(range(1,11),wcss)
+plt.xlabel("no of cluster")
+plt.ylabel("wcss")
+plt.title("Elbow Metthod")
+km=KMeans(n_clusters=5)
+km.fit(data.iloc[:,3:])
+y_pred = km.predict(data.iloc[:,3:])
+y_pred
+data["cluster"]=y_pred
+df0=data[data["cluster"]==0]
+df1=data[data["cluster"]==1]
+df2=data[data["cluster"]==2]
+df3=data[data["cluster"]==3]
+df4=data[data["cluster"]==4]
+plt.scatter(df0["Annual Income (k$)"],df0["Spending Score (1-100)"],c="red",label="cluster0")
+plt.scatter(df1["Annual Income (k$)"],df1["Spending Score (1-100)"],c="pink",label="cluster1")
+plt.scatter(df2["Annual Income (k$)"],df2["Spending Score (1-100)"],c="green",label="cluster2")
+plt.scatter(df3["Annual Income (k$)"],df3["Spending Score (1-100)"],c="blue",label="cluster3")
+plt.scatter(df4["Annual Income (k$)"],df4["Spending Score (1-100)"],c="black",label="cluster4")
+plt.legend()
+plt.title("Customer Segments")
